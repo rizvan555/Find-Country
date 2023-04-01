d4b831bf5e9c9d5e31138b73a7fed0c9473efb1e
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-04-01 16:39:50 +0200

    added fetch to product

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -6,31 +6,70 @@ const AppContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  height: 100vh;
+  background-color: aliceblue;
 
   form {
     display: flex;
     gap: 2vw;
+    margin: 20px auto 50px auto;
     input {
-      width: 20vw;
-      padding: 5px;
+      width: 30vw;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
     }
     button {
-      width: 5vw;
-      padding: 5px 20p;
+      width: 7vw;
+      padding: 10px 20p;
+      border: none;
+      border-radius: 5px;
+      box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
+
+      :hover {
+        background-color: red;
+        color: white;
+      }
+    }
+  }
+  article {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    h2 {
+      display: flex;
+      align-items: center;
+      font-weight: bold;
+      color: red;
+    }
+    ul {
+      li {
+        display: flex;
+        align-items: center;
+        list-style-type: none;
+        font-weight: bold;
+      }
+      p {
+        font-weight: 400;
+        padding-left: 10px;
+      }
     }
   }
 `;
 
-function searchButton(e: any) {
-  e.preventDefault();
-  const searchTerm = e.target.form[0].value;
-
-  fetch(`https://restcountries.com/v3.1/name/${searchTerm}`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
-
 function App() {
+  const [countryData, setCountryData] = useState<any>(null);
+
+  function searchButton(e: any) {
+    e.preventDefault();
+    const searchTerm = e.target.form[0].value;
+    fetch(`https://restcountries.com/v3.1/name/${searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => setCountryData(data[0]));
+  }
+  // )  console.log(data)  setCountryData(data[0]
   return (
     <div>
       <AppContainer>
@@ -42,7 +81,29 @@ function App() {
             <input type="text" placeholder="Country name" />
             <button onClick={searchButton}>Search</button>
           </form>
-          <article></article>
+          {countryData && (
+            <article>
+              <h2> {`${countryData.altSpellings[1]}  ${countryData.flag}`}</h2>
+              <ul>
+                <li>
+                  Capital:
+                  <p>{` ${countryData.capital}`}</p>
+                </li>
+                <li>
+                  Area:
+                  <p>{` ${countryData.area} km`}</p>
+                </li>
+                <li>
+                  Borders:
+                  <p>{` ${countryData.borders}`}</p>
+                </li>
+                <li>
+                  Google Map:
+                  <p>{` ${countryData.maps.googleMaps}`}</p>
+                </li>
+              </ul>
+            </article>
+          )}
         </main>
       </AppContainer>
     </div>
